374834fffc5abae7a2b977e743f7793aae61450c
vmartinbotter <vmartinbotter@mail.austral.edu.ar>
2026-03-18 18:11:25 -0300

    test I -> 1, feat: I devuelve 1, test pasa con lo minimo necesario

---
+++ src/tp1/RomanToArabic.java
@@ -0,0 +1,10 @@
+package tp1;
+
+public class RomanToArabic {
+    public int romanToArabic(String roman) {
+        if(roman == "I"){
+            return 1;
+        }
+        return 0;
+    }
+}
--- test/tp1/romanToArabicTest.java
+++ test/tp1/romanToArabicTest.java
@@ -1,5 +1,17 @@
-//test mas basico: si le paso I tiene que devolver 1
-@Test
-public void testRomanToArabicI() {
-    assertEquals(1, RomanToArabic.romanToArabic("I"));
+import org.junit.Test;
+import tp1.RomanToArabic;
+
+import static org.junit.Assert.assertEquals;
+
+//test mas basico: si le paso I me devuelve 1
+public class romanToArabicTest {
+    @Test
+    public void testRomanIToArabic1() {
+        RomanToArabic rta = new RomanToArabic();
+        int result = rta.romanToArabic("I");
+        assertEquals(1, result);
+    }
 }
+
+
+
