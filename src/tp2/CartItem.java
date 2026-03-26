4922371a8332397807fdcd0caafb80f77bdf437f
amgarber <agarber@mail.austral.edu.ar>
2026-03-26 08:51:58 -0300

    Test pasando con lo minimo

--- src/tp2/CartItem.java
+++ src/tp2/CartItem.java
@@ -5,18 +5,26 @@ public class CartItem {
     private int quantity;
 
     public CartItem(Product product, int quantity) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException("Quantity cannot be negative");
+        }
         this.product = product;
         this.quantity = quantity;
     }
 
-    public Product getProduct() { return product; }
-    public int getQuantity() { return quantity; }
-
     public void increaseQuantity(int quantity) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException("Quantity cannot be negative");
+        }
         this.quantity += quantity;
     }
 
     public void decreaseQuantity(int quantity) {
+        if (quantity < 0) {
+            throw new IllegalArgumentException("Quantity cannot be negative");
+        }
         this.quantity -= quantity;
     }
+    public Product getProduct() { return product; }
+    public int getQuantity() { return quantity; }
 }
